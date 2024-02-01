a011f928ed46249eef905989caa2903f815225b5
olgatenison <88651744+olgatenison@users.noreply.github.com>
2024-02-01 22:42:27 +0200

    add all tasks

--- src/baseTypes/7.ts
+++ src/baseTypes/7.ts
@@ -2,3 +2,21 @@
   Створіть функцію (isWeekend), яка приймає день тижня (з вашого enum)
   і повертає boolean значення, що вказує, чи це день робочий чи вихідний.
 */
+
+enum WeekDay {
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+  Sunday,
+}
+
+function isWeekend(day: WeekDay): boolean {
+  if ((day = WeekDay.Sunday || WeekDay.Saturday)) {
+    return true;
+  } else {
+    return false;
+  }
+}
--- src/generic/8.ts
+++ src/generic/8.ts
@@ -18,7 +18,7 @@ type Form = {
   errors: Errors;
 };
 
-// Реалізуйте Params так, щоб унеможливити поле 'errors' з типу Form
-type Params = Form;
+// Використовуючи Omit для створення типу Params без поля 'errors'
+type Params = Omit<Form, "errors">;
 
 export {};
